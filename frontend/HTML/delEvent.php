3a07520416498ac3f568b4ca580fa73e014638fc
Gaia <zhoxny07t60d912l@isisbernocchi.edu.it>
2026-05-03 16:05:41 +0200

    fix: implementato header redirect su planner post-delete

--- frontend/HTML/delEvent.php
+++ frontend/HTML/delEvent.php
@@ -6,8 +6,8 @@ $id = $_POST["id_evnt"];
 $query = "DELETE FROM events WHERE id = " . $id;
 
 if($conn->query($query) === TRUE){
-    die ("Evento eliminato con successo");
-    //header("Location: planner.html");
+    //die ("Evento eliminato con successo");
+    header("Location: planner.html");
 }else{
     echo "Errore";
 }
